9021a9f2786505838f187b9624a121c62ceebce8
danilonishimoto <daniyuzo.nishimoto@gmail.com>
2026-03-04 18:41:54 -0300

    feat: data persitance with localStorage

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import editIcon from './assets/edit-icon.svg'
 
 class App {
     constructor() {
-        this.notes = [];
+        this.notes = JSON.parse(localStorage.getItem('notes')) || [];
         this.title = '';
         this.text = '';
         this.id = '';
@@ -21,6 +21,7 @@ class App {
         this.$modalCloseButton = document.querySelector('.modal-close-button');
         this.$colorTooltip = document.querySelector('#color-tooltip');
 
+        this.render();
         this.addEventListeners();
     }
 
@@ -117,7 +118,7 @@ class App {
             id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1,
         }
         this.notes = [...this.notes, newNote];
-        this.displayNotes();
+        this.render();
         this.closeForm();
     }
 
@@ -127,14 +128,14 @@ class App {
         this.notes = this.notes.map(note =>
             note.id === Number(this.id) ? { ...note, title, text } : note
         )
-        this.displayNotes();
+        this.render();
     }
 
     editNoteColor(color) {
         this.notes = this.notes.map(note =>
             note.id === Number(this.id) ? { ...note, color } : note
         )
-        this.displayNotes();
+        this.render();
     }
 
     selectNote(event) {
@@ -153,7 +154,7 @@ class App {
         const $selectedNote = event.target.closest('.note');
         const id = $selectedNote.dataset.id;
         this.notes = this.notes.filter(note => note.id !== Number(id));
-        this.displayNotes();
+        this.render();
     }
 
     openModal(event) {
@@ -182,6 +183,15 @@ class App {
     closeTooltip(event) {
         if (!event.target.matches('.toolbar-color')) return;
         this.$colorTooltip.style.display = 'none';
+    }
+
+    render() {
+        this.saveNotes();
+        this.displayNotes();
+    }
+
+    saveNotes() {
+        localStorage.setItem('notes', JSON.stringify(this.notes))
     }
 
     displayNotes() {
